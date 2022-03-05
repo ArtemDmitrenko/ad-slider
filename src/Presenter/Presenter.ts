21e52dd0a890819c6067bc279bd7a242ad4b28dc
Artem Dmitrenko <admitrenko87@gmail.com>
2022-03-05 15:09:10 +0200

    #16 Rename folders

---
+++ src/Presenter/Presenter.ts
@@ -0,0 +1,49 @@
+import { IConfig, Model } from '../model/Model';
+import View from '../view/View';
+
+class Presenter {
+  private model: Model;
+
+  private view: View;
+
+  constructor(container: HTMLElement, options: IConfig) {
+    this.model = new Model(options);
+    this.view = new View(container);
+    this.updateView();
+    this.addObservers();
+  }
+
+  public updateView(): void {
+    this.view.updateView(this.model.options);
+  }
+
+  private addObservers(): void {
+    this.view.addObserver('changePos', this.handleCalcValue);
+    this.model.addObserver('calcPos', this.handleCalcPos);
+    this.model.addObserver('setPos', this.handleSetPos);
+  }
+
+  private handleCalcValue = (data: {
+    relPosition: number,
+    isFrom: boolean
+  }) => {
+    this.model.setValueFromHandlerPos(data);
+  }
+
+  private handleCalcPos = (options: {
+    value: number,
+    limits: { min: number; max: number },
+    isFrom: boolean
+  }) => {
+    this.view.calcPos(options);
+  }
+
+  private handleSetPos = (options: {
+    isDouble: boolean,
+    isFrom: boolean
+  }) => {
+    this.view.setPos(options);
+  }
+}
+
+export default Presenter;
---
+++ src/View/HandlerView/HandlerView.test.ts
@@ -0,0 +1,65 @@
+import HandlerView from './HandlerView';
+
+describe('handlerView', () => {
+  let parent: HTMLElement;
+  let handlerView: HandlerView;
+
+  beforeEach(() => {
+    parent = document.createElement('div');
+    handlerView = new HandlerView(parent);
+  });
+
+  test('Function render: should create element', () => {
+    expect(handlerView.handler).not.toBeNull();
+    expect(handlerView.handler.tagName).toBe('DIV');
+  });
+
+  test('Function render: should add css-class', () => {
+    expect(handlerView.handler.classList.contains('adslider__handler')).toBe(true);
+  });
+
+  test('Function render: should append track to parent-element', () => {
+    expect(handlerView.handler.parentElement).toBe(parent);
+  });
+
+  test('Function getLength: should get length of handler', () => {
+    handlerView.handler.style.width = '10px';
+    expect(handlerView.getLength()).toBe(10);
+    handlerView.handler.classList.add('adslider__handler_direction_vertical');
+    handlerView.handler.style.height = '20px';
+    expect(handlerView.getLength()).toBe(20);
+  });
+
+  test('Function setPos: should calc and set pos of handler', () => {
+    const callback1 = jest.fn();
+    const callback2 = jest.fn();
+    const callback3 = jest.fn();
+    handlerView.addObserver('calcValueNotePos', callback1);
+    handlerView.addObserver('setValueNotePos', callback2);
+    handlerView.addObserver('setBar', callback3);
+    const options = { edge: 370, value: 10, limits: { min: 0, max: 100 } };
+    handlerView.calcPos(options);
+    handlerView.setPos(false);
+    expect(window.getComputedStyle(handlerView.handler).left).toBe('37px');
+    expect(window.getComputedStyle(handlerView.handler).bottom).toBe('');
+    handlerView.setVerticalView(true);
+    handlerView.setPos(false);
+    expect(window.getComputedStyle(handlerView.handler).left).toBe('');
+    expect(window.getComputedStyle(handlerView.handler).bottom).toBe('37px');
+  });
+
+  test('Function getPos: should get position of handler', () => {
+    handlerView.handler.style.left = '15px';
+    expect(handlerView.getPos()).toBe(15);
+    handlerView.handler.classList.add('adslider__handler_direction_vertical');
+    handlerView.handler.style.bottom = '25px';
+    expect(handlerView.getPos()).toBe(25);
+  });
+
+  test('Function setVerticalView: should set vertical or horizontal view of slider', () => {
+    handlerView.setVerticalView(false);
+    expect(handlerView.handler.classList.contains('adslider__handler_direction_horizontal')).toBe(true);
+    handlerView.setVerticalView(true);
+    expect(handlerView.handler.classList.contains('adslider__handler_direction_vertical')).toBe(true);
+  });
+});
